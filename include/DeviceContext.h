4301cbf913e54bcfbe68919fa38d095544f12323
DARKETAUWU <111013710+DARKETAUWU@users.noreply.github.com>
2023-03-15 16:50:41 -0600

    Update DeviceContext.h

--- include/DeviceContext.h
+++ include/DeviceContext.h
@@ -19,70 +19,71 @@ public:
 
 	//Bind one or more render targets atomically and the depth-stencil buffer to the output-merger stage.
 	void
-		OMSetRenderTargets(	unsigned int NumViews, 
-							ID3D11RenderTargetView* const* ppRenderTargetViews, 
-							ID3D11DepthStencilView* pDepthStencilView);
+	OMSetRenderTargets(	unsigned int NumViews, 
+											ID3D11RenderTargetView* const* ppRenderTargetViews, 
+			 								ID3D11DepthStencilView* pDepthStencilView);
 
 	//Bind an array of viewports to the rasterizer stage of the pipeline.
 	void 
-		RSSetViewports(	unsigned int NumViewports, 
-						const D3D11_VIEWPORT* pViewports);
+	RSSetViewports(	unsigned int NumViewports, 
+									const D3D11_VIEWPORT* pViewports);
 
 	//Bind an input-layout object to the input-assembler stage.
 	void
-		IASetInputLayout(ID3D11InputLayout* pInputLayout);
+	IASetInputLayout(ID3D11InputLayout* pInputLayout);
 
 	//Bind an array of vertex buffers to the input-assembler stage.
 	void
-		IASetVertexBuffers(	unsigned int StartSlot, 
-							unsigned int NumBuffers, 
-							ID3D11Buffer* const* ppVertexBuffers, 
-							const unsigned int* pStrides, 
-							const unsigned int* pOffsets);
+	IASetVertexBuffers(	unsigned int StartSlot, 
+											unsigned int NumBuffers, 
+											ID3D11Buffer* const* ppVertexBuffers, 
+											const unsigned int* pStrides, 
+											const unsigned int* pOffsets);
 	//Bind an index buffer to the input-assembler stage.
 	void
-		IASetIndexBuffer(	ID3D11Buffer* pIndexBufferm, 
-							DXGI_FORMAT Format, 
-							unsigned int Offset);
+	IASetIndexBuffer(	ID3D11Buffer* pIndexBufferm, 
+										DXGI_FORMAT Format, 
+										unsigned int Offset);
 	//Bind information about the primitive type, and data order that describes input data for the input assembler stage.
 	void
-		IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY Topology);
+	IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY Topology);
 
 	//The CPU copies data from memory to a subresource created in non-mappable memory.
 	void 
-		UpdateSubresource(	ID3D11Resource* pDstResource, 
-							unsigned int DstSubresource, 
-							const D3D11_BOX* pDstBox, 
-							const void* pSrcData, 
-							unsigned int SrcRowPitch, 
-							unsigned int SrcDepthPitch);
+	UpdateSubresource(	ID3D11Resource* pDstResource, 
+											unsigned int DstSubresource, 
+											const D3D11_BOX* pDstBox, 
+											const void* pSrcData, 
+											unsigned int SrcRowPitch, 
+											unsigned int SrcDepthPitch);
 
 	//Set all the elements in a render target to one value.
 	void 
-		ClearRenderTargetView(	ID3D11RenderTargetView* pRenderTargetView, 
-								const FLOAT ColorRGBA[4]);
+	ClearRenderTargetView(	ID3D11RenderTargetView* pRenderTargetView, 
+													const FLOAT ColorRGBA[4]);
 
 	//Clears the depth-stencil resource.
 	void
-		ClearDepthStencilView(	ID3D11DepthStencilView* pDepthStencilView, 
-								unsigned int ClearFlags, 
-								float Depth, 
-								unsigned int Stencil);
+	ClearDepthStencilView(	ID3D11DepthStencilView* pDepthStencilView, 
+													unsigned int ClearFlags, 
+													float Depth, 
+													unsigned int Stencil);
+	
 	//Set a vertex shader to the device.
 	void
-		VSSetShader(ID3D11VertexShader* pVertexShader,
-					ID3D11ClassInstance* const* ppClassInstances,
-					unsigned int NumClassInstances);
+	VSSetShader(	ID3D11VertexShader* pVertexShader,
+								ID3D11ClassInstance* const* ppClassInstances,
+								unsigned int NumClassInstances);
 	//Sets the constant buffers used by the vertex shader pipeline stage.
 	void 
-		VSSetConstantBuffers(	unsigned int StartSlot, 
-								unsigned int NumBuffers, 
-								ID3D11Buffer* const* ppConstantBuffers);
+	VSSetConstantBuffers(	unsigned int StartSlot, 
+												unsigned int NumBuffers, 
+												ID3D11Buffer* const* ppConstantBuffers);
 	//Sets a pixel shader to the device.
 	void 
-		PSSetShader(	ID3D11PixelShader* pPixelShader,
-						ID3D11ClassInstance* const* ppClassInstances, 
-						unsigned int NumClassInstances);
+	PSSetShader(	ID3D11PixelShader* pPixelShader,
+							  ID3D11ClassInstance* const* ppClassInstances, 
+								unsigned int NumClassInstances);
 	//Sets the constant buffers used by the pixel shader pipeline stage.
 	void 
 		PSSetConstantBuffers(	unsigned int StartSlot, 
@@ -95,15 +96,15 @@ public:
 								ID3D11ShaderResourceView* const* ppShaderResourceViews);
 	//Set an array of sampler states to the pixel shader pipeline stage.
 	void 
-		PSSetSamplers(	unsigned int StartSlot, 
-						unsigned int NumSamplet, 
-						ID3D11SamplerState* const* ppSamplers);
+	PSSetSamplers(	unsigned int StartSlot, 
+									unsigned int NumSamplet, 
+									ID3D11SamplerState* const* ppSamplers);
 
 	//Draw indexed, non-instanced primitives.
 	void 
-		DrawIndexed(	unsigned int IndexCount, 
-						unsigned int StartIndexLocation, 
-						unsigned int BaseVertexLocation);
+	DrawIndexed(	unsigned int IndexCount, 
+								unsigned int StartIndexLocation, 
+							unsigned int BaseVertexLocation);
 	
 public:
 	//The variable with which it will be called in the entire class is referred to as many times as necessary
